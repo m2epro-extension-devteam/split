027d803b6a89b83ccfa2024fd265f2b2743154b3
Artem Vikulin <r.tem.vik@gmail.com>
2020-11-11 00:19:59 +0200

    initial

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * @author     M2E Pro Developers Team
+ * @copyright  M2E LTD
+ * @license    Commercial use is forbidden
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'M2E_Split', __DIR__);
